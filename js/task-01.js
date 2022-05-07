cc58e2739cfa78405d2a0fd2dcc428ad0304c004
Сергей <sbukij@gmail.com>
2022-05-07 11:08:32 +0300

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,10 @@
 const numberOfCategoriesEl = document.querySelectorAll(`.item`);
-console.log('Number of categories', numberOfCategoriesEl);
+console.log(`Number of categories: ${numberOfCategoriesEl.length}`);
+
+const categoryElementsEl = [...numberOfCategoriesEl]
+  .map(
+    categories => `Category: ${categories.firstElementChild.textContent}
+Elements: ${categories.children[1].children.length}
+`)
+  .join("\n");
+console.log(categoryElementsEl);
