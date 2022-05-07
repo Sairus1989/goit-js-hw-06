1e04f2a63744324c898e8bd6400082f115f00e28
Сергей <sbukij@gmail.com>
2022-05-07 12:44:54 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const elemements = ingredients.map(ingridient => {
+  const itemEl = document.createElement('li');
+  itemEl.innerHTML = ingridient;
+  itemEl.classList.add('item');
+
+  return itemEl;
+})
+listEl.append(...elemements)
+console.log(elemements);
+
+
+
+
+
+
+// let ulList = document.getElementById("ingredients");
+// const foodIngredients = ingredients.forEach(ingredient => {
+//   let items = document.createElement("li");
+//   items.innerHTML = ingredient;
+//   ulList.append(items);
+//   parent.append(...items)
+// });
+
+// console.log(foodIngredients);
