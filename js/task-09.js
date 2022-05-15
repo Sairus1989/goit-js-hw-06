a3b320d766c0420df1f0ee2c86811737f7451bd6
Сергей <sbukij@gmail.com>
2022-05-15 17:51:04 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector('body');
+const background = document.querySelector('.color');
+const btnColor = document.querySelector('.change-color');
+
+btnColor.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  const randomColor = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = randomColor;
+
+  background.textContent = randomColor;
+}
